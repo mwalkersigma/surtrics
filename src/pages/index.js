9968a276be6217064db62ab2cf9a7a817f43b263
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-09-21 16:59:13 -0500

    fix for old browser

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import {Col, Row} from "react-bootstrap";
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import Container from "react-bootstrap/Container";
 
 import useUpdates from "../modules/hooks/useUpdates";
@@ -41,9 +41,7 @@ const convertDateToDay = (date) => {
     return format(addHours(new Date(date),5),"EEE MM/dd")
 }
 
-
-
-export default function Home() {
+function HomeDisplay(){
     let date = new Date();
     date = date.toISOString().split("T")[0];
 
@@ -80,6 +78,185 @@ export default function Home() {
 
     const bestDay = Math.max(...weekData.map(({count}) => +count));
     const bestHour = Math.max(...dailyData.map(({count}) => +count));
+    return (<>
+        <Row className={"pb-3"}>
+            <Col sm={2}>
+                <InfoCard formatter={(value)=>value > goal} theme={theme} title={"Daily Total"} >
+                    {(formatter(totalForToday))}
+                </InfoCard>
+                <InfoCard formatter={(value)=>value > hourlyGoal} theme={theme} style={{marginBottom:0}} title={"Hourly total"} >
+                    {formatter(dailyData.slice(-1)[0]?.count)}
+                </InfoCard>
+            </Col>
+            <Col sm={2}>
+                <InfoCard formatter={(value)=>value > goal} theme={theme}  title={"Daily Average"}>
+                    {formatter(dailyAverage)}
+                </InfoCard>
+                <InfoCard formatter={(value)=>value > hourlyGoal} theme={theme} style={{marginBottom:0}} title={"Hourly Average"}>
+                    {formatter(hourlyAverage)}
+                </InfoCard>
+
+            </Col>
+            <Col sm={2}>
+                <InfoCard theme={theme} title={"Daily Goal"}>
+                    {formatter(goal)}
+                </InfoCard>
+                <InfoCard theme={theme} style={{marginBottom:0}} title={"Hourly Goal"}>
+                    {formatter(Math.round(hourlyGoal))}
+                </InfoCard>
+            </Col>
+            <Col sm={2}>
+                <InfoCard formatter={(value)=> value > 0} theme={theme}  title={"Daily Difference"}>
+                    {formatter(dailyDifference)}
+                </InfoCard>
+                <InfoCard formatter={(value)=> value > 0} theme={theme} style={{marginBottom:0}} title={"Hourly Difference"}>
+                    {formatter(hourlyDifference)}
+                </InfoCard>
+            </Col>
+            <Col sm={4}>
+                <BigInfoCard theme={theme} title={"Total Increments"}>
+                    {formatter(totalIncrements)}
+                </BigInfoCard>
+            </Col>
+        </Row>
+        <Row className={"pb-3"}>
+            <Col sm={4}>
+                <div
+                    className={`graph-card text-center`}
+                    style={{
+                        border: `1px solid ${theme === "dark" ? "#FFF" : "#000"}`,
+                        boxShadow: `3px 3px ${shadowColor}`,
+                        fontSize: "1.5rem",
+                    }}
+                >
+                    <p>Daily Snapshot</p>
+                    <Bar
+                        data={{
+                            labels:weekSeed.map(({date}) => convertDateToDay(date)),
+                            datasets:[
+                                {
+                                    data:weekSeed.map(({count}) => +count),
+                                    borderColor: (ctx) => ctx?.parsed?.y < goal ? colorScheme.red : colorScheme.green,
+                                    backgroundColor: (ctx) => ctx?.parsed?.y < goal ? colorScheme.red : colorScheme.green,
+                                }
+                            ]
+                        }}
+                        options={{
+                            plugins: {
+                                legend: {
+                                    display: false,
+                                },
+                                datalabels: {
+                                    color: "#FFF",
+                                },
+                            },
+                            scales: {
+                                y:{
+                                    min: 0,
+                                    max: goal * 2,
+                                    display: false,
+                                    ticks: {
+                                        color: "#FFF"
+                                    }
+                                },
+                            },
+                        }}
+                        height={190}
+                    />
+                </div>
+            </Col>
+            <Col sm={4}>
+                <BigInfoCard theme={theme} title={"Weekly Goal"}>
+                    {formatter(goal * 5)}
+                </BigInfoCard>
+            </Col>
+            <Col sm={4}>
+                <div
+                    className={"graph-card text-center"}
+                    style={{
+                        border: `1px solid ${theme === "dark" ? "#FFF" : "#000"}`,
+                        fontSize: "1.5rem",
+                        boxShadow: `3px 3px ${shadowColor}`,
+                    }}
+                >
+                    <p>Hourly Snapshot</p>
+                    <Line
+                        data={{
+                            labels: dailyData.map(({hour}) => +(subHours(new Date(hour),5).toLocaleTimeString().split(":")[0])),
+                            datasets:[
+                                {
+                                    data:dailyData.map(({count}) => +count),
+                                    borderColor:'rgba(54, 162, 235, 1)',
+                                    backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                                }
+                            ]
+                        }}
+                        options={{
+                            plugins: {
+                                legend: {
+                                    display: false,
+                                },
+                                datalabels: {
+                                    display: false,
+                                    color: theme === "dark" ? "#fff" : "#000",
+                                    align: "top",
+                                },
+                            },
+                            scales: {
+                                y:{
+                                    ticks: {
+                                        color: theme === "dark" ? "#FFF" : "#000"
+                                    }
+                                },
+                            },
+                        }}
+                        height={190}
+                    />
+                </div>
+            </Col>
+        </Row>
+        <Row style={{marginBottom:'5rem'}}>
+            <Col sm={4}>
+                <BigInfoCard formatter={(value)=>value > goal} theme={theme} title={"Best Day"}>
+                    {formatter(bestDay)}
+                </BigInfoCard>
+            </Col>
+            <Col sm={2} className={"gap-2"}>
+                <InfoCard theme={theme} title={"Planned Total"} subtitle={"By end of day"}>
+                    {formatter(expectedTotal)}
+                </InfoCard>
+                <InfoCard formatter={()=> totalDifference < 0} theme={theme} style={{marginBottom:0}} title={"Total Difference"}>
+                    {formatter(totalDifference) > 0 ? formatter(totalDifference):0}
+                </InfoCard>
+            </Col>
+            <Col sm={2}>
+                <InfoCard formatter={(value)=> value <= 0} theme={theme} title={"Best VS Today"} >
+                    {formatter(bestDay - totalForToday)}
+                </InfoCard>
+                <InfoCard formatter={(value)=>{
+                    return bestHour - dailyData.slice(-1)[0]?.count <= 0
+                }} theme={theme} style={{marginBottom:0}} title={"Best HR VS Now"}>
+                    {formatter(bestHour - dailyData.slice(-1)[0]?.count)}
+                </InfoCard>
+            </Col>
+            <Col sm={4}>
+                <BigInfoCard formatter={(value)=>value > hourlyGoal} theme={theme} title={"Best Hour"}>
+                    {formatter(bestHour)}
+                </BigInfoCard>
+            </Col>
+        </Row>
+    </>)
+}
+
+
+export default function Home() {
+    const [hasNavBar, setHasNavBar] = useState(true);
+    useEffect(() => {
+        if (typeof window === 'undefined') return;
+        const urlParams = new URLSearchParams(window.location.search);
+        const nav = urlParams.get('nav');
+        if(nav === "false") setHasNavBar(false);
+    }, []);
 
 
 
@@ -90,177 +267,10 @@ export default function Home() {
         <meta name="description" content="Generated by create next app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-        <Container className={"mt-5"}>
+        {hasNavBar && <Container className={"mt-5"}>
             <h3 className={"text-center"}>Surplus Metrics Dashboard</h3>
-        </Container>
-        <Container className={"mt-5"} >
-            <Row className={"pb-3"}>
-                <Col sm={2}>
-                    <InfoCard formatter={(value)=>value > goal} theme={theme} title={"Daily Total"} >
-                        {(formatter(totalForToday))}
-                    </InfoCard>
-                    <InfoCard formatter={(value)=>value > hourlyGoal} theme={theme} style={{marginBottom:0}} title={"Hourly total"} >
-                        {formatter(dailyData.slice(-1)[0]?.count)}
-                    </InfoCard>
-                </Col>
-                <Col sm={2}>
-                    <InfoCard formatter={(value)=>value > goal} theme={theme}  title={"Daily Average"}>
-                        {formatter(dailyAverage)}
-                    </InfoCard>
-                    <InfoCard formatter={(value)=>value > hourlyGoal} theme={theme} style={{marginBottom:0}} title={"Hourly Average"}>
-                        {formatter(hourlyAverage)}
-                    </InfoCard>
-
-                </Col>
-                <Col sm={2}>
-                    <InfoCard theme={theme} title={"Daily Goal"}>
-                        {formatter(goal)}
-                    </InfoCard>
-                    <InfoCard theme={theme} style={{marginBottom:0}} title={"Hourly Goal"}>
-                        {formatter(Math.round(hourlyGoal))}
-                    </InfoCard>
-                </Col>
-                <Col sm={2}>
-                    <InfoCard formatter={(value)=> value > 0} theme={theme}  title={"Daily Difference"}>
-                        {formatter(dailyDifference)}
-                    </InfoCard>
-                    <InfoCard formatter={(value)=> value > 0} theme={theme} style={{marginBottom:0}} title={"Hourly Difference"}>
-                        {formatter(hourlyDifference)}
-                    </InfoCard>
-                </Col>
-                <Col sm={4}>
-                    <BigInfoCard theme={theme} title={"Total Increments"}>
-                        {formatter(totalIncrements)}
-                    </BigInfoCard>
-                </Col>
-            </Row>
-            <Row className={"pb-3"}>
-                <Col sm={4}>
-                    <div
-                        className={`graph-card text-center`}
-                        style={{
-                            border: `1px solid ${theme === "dark" ? "#FFF" : "#000"}`,
-                            boxShadow: `3px 3px ${shadowColor}`,
-                            fontSize: "1.5rem",
-                        }}
-                    >
-                        <p>Daily Snapshot</p>
-                        <Bar
-                            data={{
-                                labels:weekSeed.map(({date}) => convertDateToDay(date)),
-                                datasets:[
-                                    {
-                                        data:weekSeed.map(({count}) => +count),
-                                        borderColor: (ctx) => ctx?.parsed?.y < goal ? colorScheme.red : colorScheme.green,
-                                        backgroundColor: (ctx) => ctx?.parsed?.y < goal ? colorScheme.red : colorScheme.green,
-                                    }
-                                ]
-                            }}
-                            options={{
-                                plugins: {
-                                    legend: {
-                                        display: false,
-                                    },
-                                    datalabels: {
-                                        color: "#FFF",
-                                    },
-                                },
-                                scales: {
-                                    y:{
-                                        min: 0,
-                                        max: goal * 2,
-                                        display: false,
-                                        ticks: {
-                                            color: "#FFF"
-                                        }
-                                    },
-                                },
-                            }}
-                            height={190}
-                        />
-                    </div>
-                </Col>
-                <Col sm={4}>
-                    <BigInfoCard theme={theme} title={"Weekly Goal"}>
-                        {formatter(goal * 5)}
-                    </BigInfoCard>
-                </Col>
-                <Col sm={4}>
-                    <div
-                        className={"graph-card text-center"}
-                        style={{
-                            border: `1px solid ${theme === "dark" ? "#FFF" : "#000"}`,
-                            fontSize: "1.5rem",
-                            boxShadow: `3px 3px ${shadowColor}`,
-                        }}
-                    >
-                        <p>Hourly Snapshot</p>
-                        <Line
-                            data={{
-                                labels: dailyData.map(({hour}) => +(subHours(new Date(hour),5).toLocaleTimeString().split(":")[0])),
-                                datasets:[
-                                    {
-                                        data:dailyData.map(({count}) => +count),
-                                        borderColor:'rgba(54, 162, 235, 1)',
-                                        backgroundColor: 'rgba(54, 162, 235, 0.5)',
-                                    }
-                                ]
-                            }}
-                            options={{
-                                plugins: {
-                                    legend: {
-                                        display: false,
-                                    },
-                                    datalabels: {
-                                        display: false,
-                                        color: theme === "dark" ? "#fff" : "#000",
-                                        align: "top",
-                                    },
-                                },
-                                scales: {
-                                    y:{
-                                        ticks: {
-                                            color: theme === "dark" ? "#FFF" : "#000"
-                                        }
-                                    },
-                                },
-                            }}
-                            height={190}
-                        />
-                    </div>
-                </Col>
-            </Row>
-            <Row style={{marginBottom:'5rem'}}>
-                <Col sm={4}>
-                    <BigInfoCard formatter={(value)=>value > goal} theme={theme} title={"Best Day"}>
-                        {formatter(bestDay)}
-                    </BigInfoCard>
-                </Col>
-                <Col sm={2} className={"gap-2"}>
-                    <InfoCard theme={theme} title={"Planned Total"} subtitle={"By end of day"}>
-                        {formatter(expectedTotal)}
-                    </InfoCard>
-                    <InfoCard formatter={()=> totalDifference < 0} theme={theme} style={{marginBottom:0}} title={"Total Difference"}>
-                       {formatter(totalDifference) > 0 ? formatter(totalDifference):0}
-                    </InfoCard>
-                </Col>
-                <Col sm={2}>
-                    <InfoCard formatter={(value)=> value <= 0} theme={theme} title={"Best VS Today"} >
-                        {formatter(bestDay - totalForToday)}
-                    </InfoCard>
-                    <InfoCard formatter={(value)=>{
-                        return bestHour - dailyData.slice(-1)[0]?.count <= 0
-                    }} theme={theme} style={{marginBottom:0}} title={"Best HR VS Now"}>
-                        {formatter(bestHour - dailyData.slice(-1)[0]?.count)}
-                    </InfoCard>
-                </Col>
-                <Col sm={4}>
-                    <BigInfoCard formatter={(value)=>value > hourlyGoal} theme={theme} title={"Best Hour"}>
-                        {formatter(bestHour)}
-                    </BigInfoCard>
-                </Col>
-            </Row>
-        </Container>
+        </Container>}
+        <Container className={`${hasNavBar ? "mt-5" : ""}`} ><HomeDisplay/></Container>
     </>
   )
 }

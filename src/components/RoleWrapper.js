1594f2709e6bfc9e153c2e98abc0af1354f607c7
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-10-30 09:40:19 -0500

    Fixed a bug with the role wrapper not allowing multiple roles access.

--- src/components/RoleWrapper.js
+++ src/components/RoleWrapper.js
@@ -23,9 +23,20 @@ function RoleWrapper({children,altRoles,invisible}){
     if(status === "loading" || !adminList )return<Container><h2>Loading</h2></Container>;
     if(status === "error") return <Container><h2>Error</h2></Container>;
     if(!session)return <Container><h2>Not logged in</h2></Container>;
-        if(Array.isArray(altRoles)) {
+    if(Array.isArray(altRoles)) {
+        console.log(altRoles);
         let hasAuthorizedRole = altRoles.map(role => isRole(role)(session)).includes(true);
-        if (!isAdmin(session) && !hasAuthorizedRole) return <Container><h2 className={"text-center"}>Not an Authorized User.</h2><p className={"text-center"}>If you feel like you should be reach out to michael walker</p></Container>;
+        console.log(hasAuthorizedRole)
+        console.log(isAdmin(session))
+        if (!isAdmin(session) && !hasAuthorizedRole) return <Container>
+            <h2 className={"text-center"}>
+                Not an Authorized User.
+            </h2>
+            <p className={"text-center"}>
+                If you feel like you should be reach out to michael walker
+            </p>
+        </Container>;
+        return children;
     }
     if(isAdmin(session) || (altRoles && isRole(altRoles)(session))) return children;
     return <Container><h2 className={"text-center"}>Not an Authorized User.</h2><p className={"text-center"}>If you feel like you should be reach out to michael walker</p></Container>;

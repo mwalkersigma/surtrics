da63a66f7281e1ebaf2de19f1fa5fe9240e587e9
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-11-15 11:32:32 -0600

    Fixing Bug with dates

--- src/components/RoleWrapper.js
+++ src/components/RoleWrapper.js
@@ -16,27 +16,30 @@ function RoleWrapperInvisible({children,altRoles}){
     }
     if(isAdmin(session) || (altRoles && isRole(altRoles)(session))) return children;
 }
-function RoleWrapper({children,altRoles,invisible}){
+function Loading(){
+    return <Container><h2>Loading</h2></Container>;
+}
+function Unauthorized(){
+    return <Container>
+        <h2 className={"text-center"}>Not an Authorized User.</h2>
+        <p className={"text-center"}>If you feel like you should be reach out to michael walker</p>
+    </Container>;
+
+}
+function RoleWrapper({children,altRoles,invisible,LoadingComponent = <Loading/>}){
     const {data:session, status} = useSession();
     const {adminList, isAdmin,isRole} = useAdminList();
     if(invisible)return <RoleWrapperInvisible altRoles={altRoles}>{children}</RoleWrapperInvisible>
-    if(status === "loading" || !adminList )return<Container><h2>Loading</h2></Container>;
+    if(status === "loading" || !adminList ) return<>{LoadingComponent}</>  ;
     if(status === "error") return <Container><h2>Error</h2></Container>;
     if(!session)return <Container><h2>Not logged in</h2></Container>;
     if(Array.isArray(altRoles)) {
         let hasAuthorizedRole = altRoles.map(role => isRole(role)(session)).includes(true);
-        if (!isAdmin(session) && !hasAuthorizedRole) return <Container>
-            <h2 className={"text-center"}>
-                Not an Authorized User.
-            </h2>
-            <p className={"text-center"}>
-                If you feel like you should be reach out to michael walker
-            </p>
-        </Container>;
+        if (!isAdmin(session) && !hasAuthorizedRole) return <Unauthorized/>;
         return children;
     }
     if(isAdmin(session) || (altRoles && isRole(altRoles)(session))) return children;
-    return <Container><h2 className={"text-center"}>Not an Authorized User.</h2><p className={"text-center"}>If you feel like you should be reach out to michael walker</p></Container>;
+    return <Unauthorized/>;
 }
 
 export default RoleWrapper;

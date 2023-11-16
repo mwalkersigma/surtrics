e46d2991c5887b678ba24aef6b2cec66dbe4ca51
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-11-16 14:40:20 -0600

    Finished Updating Daily View

--- src/pages/graphs/increments/dailyView.js
+++ src/pages/graphs/increments/dailyView.js
@@ -1,31 +1,32 @@
-import React, {useContext, useState} from 'react';
-import Form from "react-bootstrap/Form";
-import useUpdates from "../../../modules/hooks/useUpdates";
-import LineGraph from "../../../components/lineGraph";
-import {ThemeContext} from "../../layout";
+import React, {useState} from 'react';
 
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import formatDateWithZeros from "../../../modules/utils/formatDateWithZeros";
+import LineGraph from "../../../components/lineGraph";
 import InfoCard from "../../../components/infoCards/infocard";
+
+import useUpdates from "../../../modules/hooks/useUpdates";
+import formatter from "../../../modules/utils/numberFormatter";
+
+
+
+import {Container, SimpleGrid, Stack, useMantineColorScheme, Grid, Space, Skeleton,Title} from "@mantine/core";
+
 import {
     CategoryScale,
     Chart as ChartJS,
     Legend,
     LinearScale,
     LineElement, PointElement,
-    Title,
     Tooltip
 } from "chart.js";
-import formatter from "../../../modules/utils/numberFormatter";
+import {DatePickerInput} from "@mantine/dates";
+import StatsCard from "../../../components/mantine/StatsCard";
+
 
 
 
 ChartJS.register(
     CategoryScale,
     LinearScale,
-    Title,
     Tooltip,
     Legend,
     LineElement,
@@ -33,43 +34,114 @@ ChartJS.register(
 );
 
 const DailyView = () => {
-    const [date,setDate] = useState(formatDateWithZeros(new Date()))
+    const [date,setDate] = useState(new Date())
     let dailyData = useUpdates("/api/views/increments",{date, interval:"1 day", increment: "hour"});
-    console.log(dailyData)
-    const theme = useContext(ThemeContext)
+    const {colorScheme:theme} = useMantineColorScheme();
+
     if(dailyData.length === 0)return(
-        <Container className={"text-center"}>
-            <Form.Control className={"mb-5"} value={date} onChange={(e)=>setDate(e.target.value)} type="date" />
-            Loading... ( If this takes more than 10 seconds, there is probably no data for this date )
+        <Container fluid>
+            <Title ta={"center"}>Surplus Increments Daily View</Title>
+            <Grid spacing={"xl"}>
+                <Grid.Col span={1}></Grid.Col>
+                <Grid.Col span={8}>
+                    <DatePickerInput
+                        mt={"xl"}
+                        mb={"xl"}
+                        label={"Date"}
+                        value={date}
+                        onChange={(e)=>setDate(e)}
+                    />
+                </Grid.Col>
+                <Grid.Col span={3}></Grid.Col>
+            </Grid>
+            <Grid spacing={"xl"}>
+                <Grid.Col span={1}></Grid.Col>
+                <Grid.Col span={8}>
+                    <Skeleton height={"65vh"} radius="md" animate={false} />
+                </Grid.Col>
+                <Grid.Col span={2}>
+                    <Stack>
+                        <Skeleton height={110} radius="md" animate={false} />
+                        <Skeleton height={110} radius="md" animate={false} />
+                        <Skeleton height={110} radius="md" animate={false} />
+                        <Skeleton height={110} radius="md" animate={false} />
+                        <Skeleton height={110} radius="md" animate={false} />
+                    </Stack>
+                </Grid.Col>
+            </Grid>
         </Container>
     );
-    dailyData = dailyData.map(({count}) => +count);
-    let margin = "3rem";
-    return (
-        <Container>
-            <Form.Control
-                className={"mb-3"}
-                value={date}
-                onChange={(e)=>setDate(e.target.value)}
-                type="date"
-            />
-            <Row>
-                <Col sm={10} className={`themed-drop-shadow ${theme}`} style={{border:`1px ${theme === "dark" ? "white" : "black" } solid`}} >
-                    <LineGraph title={"Daily View"} height={150} date={date} dailyData={dailyData} theme={theme} />
-                </Col>
-                <Col sm={2}>
-                    <InfoCard style={{marginBottom:margin}}  title={"Total"} theme={theme}>
-                        {formatter(dailyData.reduce((a,b) => a + b,0))}
-                    </InfoCard>
-                    <InfoCard style={{marginBottom:margin}} title={"Average"} theme={theme}>
-                        {formatter(Math.round(dailyData.reduce((a,b) => a + b,0) / dailyData.length))}
-                    </InfoCard>
-                    <InfoCard style={{marginBottom:0}} title={"Best Hour"} theme={theme}>
-                        {formatter(dailyData.reduce((a,b) => a > b ? a : b,0))}
-                    </InfoCard>
-                </Col>
-            </Row>
 
+    let chartData = dailyData
+        .reduce((acc,curr) => {
+            let date = new Date(curr.date_of_transaction).toLocaleString().split("T")[0];
+            if (!acc[date]) acc[date] = 0;
+            acc[date] += +curr.count;
+            return acc
+        },{})
+    chartData = Object.entries(chartData).map(([date,count]) => ({date_of_transaction:date,count}));
+    chartData = chartData.map(({count}) => +count);
+
+    let margin = "1rem";
+    return (
+        <Container fluid>
+            <Title ta={"center"}>Surplus Increments Daily View</Title>
+            <Grid spacing={"xl"}>
+                <Grid.Col span={1}></Grid.Col>
+                <Grid.Col span={8}>
+                    <DatePickerInput
+                        mt={"xl"}
+                        mb={"xl"}
+                        label={"Date"}
+                        value={date}
+                        onChange={(e)=>setDate(e)}
+                    />
+                </Grid.Col>
+                <Grid.Col span={3}></Grid.Col>
+            </Grid>
+            <Grid spacing={"xl"}>
+                <Grid.Col span={1}></Grid.Col>
+                <Grid.Col span={8}>
+                    <div style={{position: "relative", height:"100%"}} className={"chart-container"}>
+                        <LineGraph date={date} dailyData={chartData} theme={theme} />
+                    </div>
+                </Grid.Col>
+                <Grid.Col span={2}>
+                    <Stack>
+                        <StatsCard
+                            stat={{
+                                title:"Total",
+                                value:formatter(chartData.reduce((a,b) => a + b,0)),
+                            }}/>
+                        <StatsCard
+                            stat={{
+                                title:"Average",
+                                value:formatter(Math.round(chartData.reduce((a,b) => a + b,0) / dailyData.length)),
+                            }}/>
+                        <StatsCard
+                            stat={{
+                                title:"Best Hour",
+                                value:formatter(chartData.reduce((a,b) => a > b ? a : b,0)),
+                            }}/>
+                        <StatsCard
+                            stat={{
+                                title:"New Inbound",
+                                value:formatter(dailyData
+                                    .filter((item) => (item.transaction_reason === "Add on Receiving" || item.transaction_reason === "Add"))
+                                    .reduce((acc, {count})=>acc + +count ,0)),
+                            }}
+                        />
+
+                        <StatsCard
+                            stat={{
+                                title:"Re-listings",
+                                value:formatter(dailyData
+                                    .filter((item) => (item.transaction_reason === "Relisting"))
+                                    .reduce((acc, {count})=>acc + +count ,0)),
+                            }}/>
+                    </Stack>
+                </Grid.Col>
+            </Grid>
         </Container>
     );
 };
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -86,7 +86,7 @@ export default function Layout({children}) {
                                     className={cx(classes.user, {[classes.userActive]: userMenuOpened})}
                                 >
                                     <Group gap={7}>
-                                        <Avatar src={user.image} alt={user.name} radius="xl" size={20}/>
+                                        <Avatar imageProps={{referrerPolicy:"no-referrer"}} src={user.image} alt={user.name} radius="xl" size={20}/>
                                         <Text fw={500} size="sm" lh={1} mr={3}>
                                             {user.name}
                                         </Text>

9ed4f7c21342130295725f92b336da1ff3a7c0a5
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-11-20 10:16:55 -0600

    finished the ebay data entry screen

--- src/pages/BSA/EbayEntry.js
+++ src/pages/BSA/EbayEntry.js
@@ -1,113 +1,107 @@
 import React, {useEffect, useState} from 'react';
 import RoleWrapper from "../../components/RoleWrapper";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Stack from "react-bootstrap/Stack";
-import Button from "react-bootstrap/Button";
+
 import {useSession} from "next-auth/react";
 import formatDateWithZeros from "../../modules/utils/formatDateWithZeros";
 import {addDays} from "date-fns";
 import findStartOfWeek from "../../modules/utils/findSundayFromDate";
-import createSuccessMessage from "../../modules/serverMessageFactories/createSuccessMessage";
-import useToastContainer from "../../modules/hooks/useToast";
-import ToastContainerWrapper from "../../components/toast/toastContainerWrapper";
-import createErrorMessage from "../../modules/serverMessageFactories/createErrorMessage";
-import useBasicForm from "../../modules/hooks/useBasicForm";
-
+import {useForm} from "@mantine/form";
+import {Notifications} from "@mantine/notifications";
+import {Button, Container, Grid, NumberInput, Stack, TextInput, Title} from "@mantine/core";
+import {DatePickerInput} from "@mantine/dates";
+import {values} from "pg/lib/native/query";
 
 
 const EbayEntry = () => {
-    const {data: session} = useSession();
+    const {data: session,status} = useSession();
     const userName = session?.user?.name;
-    const [serverMessages, setServerMessage, removeServerMessage] = useToastContainer();
-    const [formState, handleChange,handleReset] = useBasicForm({
-        impressions: 0,
-        page_views: 0,
-        date_for_week: formatDateWithZeros(addDays(findStartOfWeek(new Date()),1)),
+
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm({
+        initialValues: {
+            impressions: 0,
+            page_views: 0,
+            date_for_week: new Date(),
+            user_who_submitted: userName || "",
+        }
     })
 
-    function handleDate(e){
-        let update = {target:{value:formatDateWithZeros(addDays(findStartOfWeek(new Date(e.target.value)),1))}};
-        handleChange("date_for_week")
-        (update);
-    }
-    /*
-    *
-    * ebay_impression
-    * ebay page_views
-    * ebay_sold
-    * ebay_click_through_rate
-    * ebay_conversion
-    * ebay_sales
-     */
-    function handleSubmit(){
-        fetch(`${window.location.origin}/api/dataEntry/ebay`,{
-            method:"PUT",
-            body:JSON.stringify(formState)
+    useEffect(() => {
+        if(status === "loading"){
+            setLoading(true)
+        }
+        if(status === "authenticated"){
+            setLoading(false)
+        }
+        let values = {user_who_submitted: userName}
+        form.setValues((prevValues) => ({...prevValues, ...values}));
+        form.resetDirty(values)
+
+    }, [status]);
+    function handleSubmit(values) {
+        setLoading(true)
+        fetch(`${window.location.origin}/api/dataEntry/ebay`, {
+            method: "PUT",
+            body: JSON.stringify(values)
         })
-            .then((res)=>res.json())
-            .then(({message,response})=>{
+            .then((res) => res.json())
+            .then(({message, response}) => {
                 console.log(response)
-                setServerMessage(createSuccessMessage(message))
-                handleReset();
+                Notifications.show({autoClose: 5000, title: "Success", message, type: "success"})
             })
-            .catch((err)=>{
-                setServerMessage(createErrorMessage(err.message))
+            .catch((err) => {
+                Notifications.show({autoClose: 5000, title: "Error", message: err.message, type: "error"})
+            })
+            .finally(() => {
+                setLoading(false)
+                form.reset()
             })
     }
     return (
         <RoleWrapper altRoles={"bsa"}>
-            <ToastContainerWrapper serverMessages={serverMessages} removeServerMessages={removeServerMessage}/>
             <Container>
-                <h1 className={"text-center my-4"}>Ebay </h1>
-                <Form>
-                    <Row className={"my-3"}>
-                        <Form.Group as={Col} controlId={"form_ebay_impression"}>
-                            <Form.Label>Impression</Form.Label>
-                            <Form.Control
-                                type={"number"}
-                                placeholder={0}
-                                value={formState.impressions}
-                                onChange={handleChange("impressions")}
-                            />
-                        </Form.Group>
-                        <Form.Group as={Col} controlId={"form_ebay_page_views"}>
-                            <Form.Label>Page Views</Form.Label>
-                            <Form.Control
-                                type={"number"}
-                                placeholder={0}
-                                value={formState.page_views}
-                                onChange={handleChange("page_views")}
-                            />
-                        </Form.Group>
-                        <Form.Group as={Col} controlId={"form_control_for_week"}>
-                            <Form.Label>for week</Form.Label>
-                            <Form.Control
-                                type={"date"}
-                                value={formState.date_for_week}
-                                onChange={handleDate}
-                            />
-                        </Form.Group>
-                    </Row>
-                    <Row className={"my-3"}>
-                        <Form.Group as={Col} controlId={"form_control_user_who_submitted"}>
-                            <Form.Label>Who Submitted</Form.Label>
-                            <Form.Control
-                                type={"text"}
-                                disabled
-                                readOnly
-                                placeholder={userName}
-                            />
-                        </Form.Group>
-                        <Col>
-                            <Stack className={"justify-content-end align-items-lg-stretch h-100"}>
-                                <Button onClick={handleSubmit} variant={"primary"}>Submit</Button>
-                            </Stack>
-                        </Col>
-                    </Row>
-                </Form>
+                <form onSubmit={form.onSubmit(handleSubmit)}>
+                    <Grid>
+                    <Grid.Col span={12}>
+                        <Title> Ebay </Title>
+                    </Grid.Col>
+                    <Grid.Col span={4}>
+                        <NumberInput
+                            label={"Impressions"}
+                            {...form.getInputProps("impressions")}
+                        />
+                    </Grid.Col>
+
+                    <Grid.Col span={4}>
+                        <NumberInput
+                            label={"Page Views"}
+                            {...form.getInputProps("page_views")}
+                        />
+                    </Grid.Col>
+
+                    <Grid.Col span={4}>
+                        <DatePickerInput
+                            label={"Date"}
+                            {...form.getInputProps("date_for_week")}
+                        />
+                    </Grid.Col>
+
+                    <Grid.Col span={6}>
+                        <TextInput
+                            label={"Who Submitted"}
+                            disabled
+                            readOnly
+                            value={form.values.user_who_submitted}
+                        />
+                    </Grid.Col>
+                    <Grid.Col span={6}>
+                        <Stack h={"100%"} justify={"flex-end"}>
+                            <Button loading={loading} type={"submit"} variant={"primary"}>Submit</Button>
+                        </Stack>
+                    </Grid.Col>
+                </Grid>
+                </form>
             </Container>
         </RoleWrapper>
     );

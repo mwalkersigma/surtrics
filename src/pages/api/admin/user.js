13d3f765ae3ed0960097a610bc56565cff38ff1b
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-11-22 12:32:17 -0600

    Last changes

--- src/pages/api/admin/user.js
+++ src/pages/api/admin/user.js
@@ -2,40 +2,45 @@ import serverAdminWrapper from "../../../modules/auth/serverAdminWrapper";
 import router from "../../../modules/serverUtils/requestRouter";
 import {parseBody} from "../../../modules/serverUtils/parseBody";
 import fs from "fs";
+import {server} from "next-auth/client/__tests__/helpers/mocks";
 
 function getHandler(req,res,...options){
     let userList = fs.readFileSync("./src/json/adminList.json")
         userList = JSON.parse(`${userList}`);
-        console.log(userList)
     return userList;
 }
 function postHandler(req,res,...options){
-    let userList = fs.readFileSync("./src/json/users.json")
+    serverAdminWrapper((req)=>{
+        let userList = fs.readFileSync("./src/json/users.json")
         userList = JSON.parse(`${userList}`);
 
-    let email = req.body.user.email;
+        let email = req.body.user.email;
 
-    let emails = userList.map((user)=>user.email);
-    if(emails.includes(email)){
-        return {message:"User already exists"}
-    }
+        let emails = userList.map((user)=>user.email);
+        if(emails.includes(email)){
+            return {message:"User already exists"}
+        }
 
-    userList.push({
-        email:req.body.user.email,
-        name:req.body.user.name,
-        roles:req.body.user.roles
-    });
+        userList.push({
+            email:req.body.user.email,
+            name:req.body.user.name,
+            roles:req.body.user.roles
+        });
 
-    fs.writeFileSync("./src/json/users.json",JSON.stringify(userList));
+        fs.writeFileSync("./src/json/users.json",JSON.stringify(userList));
 
-    return {message:"User added"}
+        return {message:"User added"}
+    })(req,res,...options)
+
 }
 function putHandler(req,res,...options){
     // update all users
-    let newState = parseBody(req)['newState'];
+    serverAdminWrapper((req)=>{
+        let newState = parseBody(req)['newState'];
 
-    fs.writeFileSync("./src/json/adminList.json",JSON.stringify(newState,null,2));
-    return newState
+        fs.writeFileSync("./src/json/adminList.json",JSON.stringify(newState,null,2));
+        return newState
+    })(req,res,...options)
 }
 function patchHandler(req,res,...options){
     // update single user
@@ -43,33 +48,33 @@ function patchHandler(req,res,...options){
 }
 function deleteHandler(req,res,...options){
     // delete single user
-    req.body = parseBody(req);
-    let adminList = fs.readFileSync("./src/json/adminList.json")
+    return serverAdminWrapper((req,res)=>{
+        req.body = parseBody(req);
+        let adminList = fs.readFileSync("./src/json/adminList.json")
         adminList = JSON.parse(`${adminList}`);
 
-    let email = req.body.email;
+        let email = req.body.email;
 
-    let emails = adminList.map((user)=>user.email);
-    if(!emails.includes(email)){
-        return {message:"User does not exist"}
-    }
+        let emails = adminList.map((user)=>user.email);
+        if(!emails.includes(email)){
+            return {message:"User does not exist"}
+        }
 
-    adminList = adminList.filter((user)=>user.email !== email);
+        adminList = adminList.filter((user)=>user.email !== email);
 
-    fs.writeFileSync("./src/json/adminList.json",JSON.stringify(adminList,null,2));
+        fs.writeFileSync("./src/json/adminList.json",JSON.stringify(adminList,null,2));
 
-    return "User deleted"
+        return "User deleted"
+    })(req,res)
 }
 
 export default function handler ( req, res ) {
-    return serverAdminWrapper((req, res) => {
-        return router({
-            GET: getHandler,
-            POST: postHandler,
-            PUT: putHandler,
-            PATCH: patchHandler,
-            DELETE: deleteHandler,
-        })(req, res)
+    return router({
+        GET: getHandler,
+        POST: postHandler,
+        PUT: putHandler,
+        PATCH: patchHandler,
+        DELETE: deleteHandler,
     })(req, res)
         .then((response) => {
             res.status(200).json(response);

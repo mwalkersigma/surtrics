28c3fcb0c440b1429495ae7b037d39aa91334fe6
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2024-01-10 14:45:11 -0600

    Add totalOnly API endpoint for quantity view

---
+++ src/pages/api/views/quantity/totalOnly.js
@@ -0,0 +1,55 @@
+import db from "../../../../db/index"
+import router from "../../../../modules/serverUtils/requestRouter";
+import {parseBody} from "../../../../modules/serverUtils/parseBody";
+import Query from "../../../../modules/classes/query";
+
+
+
+
+async function postHandler (req,res) {
+    let body = parseBody(req);
+    let query = new Query("surtrics.surplus_metrics_data",[
+        `sum(quantity)`,
+        `"user" as name`,
+        `transaction_type as type`,
+    ])
+        .addWhere(`"user"`, `!=`, 'BSA')
+        .addWhere('transaction_type', '=', 'Add')
+        .addWhereWithOr([
+            {column:'transaction_reason',operator:'=',value:'Relisting'},
+            {column:'transaction_reason',operator:'=',value:'Add'},
+            {column:'transaction_reason',operator:'=',value:'Add on Receiving'},
+        ])
+        .addGroupBy("name")
+        .addGroupBy("type")
+        .addGroupBy("date")
+        .addOrderBy("date",'ASC')
+        .conditional(body.date,
+            ()=>query.addWhere('transaction_date','>=',body.date),
+            ()=>null
+        )
+        .conditional(body.startDate && body.endDate,
+            ()=>query.addWhere('transaction_date','>=',body.startDate)
+                .addWhere('transaction_date','<=',body.endDate),
+            ()=>null
+        )
+        .conditional(body.interval,
+            ()=>query.addAggregate(`DATE_TRUNC('@', transaction_date) as date`,body.interval),
+            ()=>query.addColumn(`DATE_TRUNC('week',transaction_date) as date`)
+        )
+    return db.query(query.query, query.params)
+        .then((response) => {
+            res.status(200).json(response.rows)
+        })
+        .catch((error) => {
+            res.status(500).json(error)
+        })
+
+
+}
+
+
+export default router({
+    POST:postHandler
+})
+

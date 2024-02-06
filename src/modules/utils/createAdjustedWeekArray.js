e9b66bc60bd6d9fcc632e95e2ad617987fe2c711
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2024-02-06 08:58:57 -0600

    bug fix for weekly increments view

--- src/modules/utils/createAdjustedWeekArray.js
+++ src/modules/utils/createAdjustedWeekArray.js
@@ -1,20 +1,28 @@
 export default function createAdjustedWeekArray(data, goal) {
+    // Week Goal
+    // Takes the daily goal and multiplies it by the number of days that have been tracked
+    // This gives us the running total of the goal for the week
     let weekGoal = goal * data.filter(day=> day.count !== 0).length
+    // weekTotal
+    // Takes the data and reduces it to a single value
+    // This gives us the running total of the count for the week
     let weekTotal = data.reduce((acc, curr) => {
         return acc + curr.count;
     },0);
+    console.log("weekGoal", weekGoal);
+    console.log("weekTotal", weekTotal);
 
-    let days = data.filter(day=> day.count !== 0).length;
+    let days = data.filter(day=> day.count !== 0).length + 1;
     let daysRemaining = 5 - days;
     daysRemaining = daysRemaining <= 0 ? 1 : daysRemaining;
 
-    let baseWeek = Array(5).fill(0);
+    let baseWeek = Array(days).fill(0);
 
     let weekDifference = (weekGoal - weekTotal) / daysRemaining;
 
     if(weekDifference > 0) {
-        baseWeek.splice(5 - daysRemaining, daysRemaining, ...Array(daysRemaining).fill(weekDifference));
+        baseWeek.splice(days - daysRemaining, daysRemaining, ...Array(daysRemaining).fill(weekDifference));
     }
-
+    console.log("baseWeek", baseWeek);
     return baseWeek;
 }

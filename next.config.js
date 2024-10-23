d8dc543bf0af395e16af3e88ce0365071c508000
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2024-10-23 12:02:18 -0500

    fix: moving metrics out of page directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+/** @type {import('next').NextConfig} */
+const {
+    PHASE_PRODUCTION_BUILD,
+    PHASE_PRODUCTION_SERVER,
+} = require("next/constants");
 
-
-module.exports = {
-    reactStrictMode: true,
-    distDir: 'build'
+module.exports = (phase) => {
+    let baseConfig = {
+        reactStrictMode: true,
+    }
+    let distDir = "";
+    if (phase === PHASE_PRODUCTION_BUILD) {
+        distDir = "build";
+    }
+    else if (phase === PHASE_PRODUCTION_SERVER) {
+        distDir = ".next";
+    }
+    baseConfig.distDir = distDir;
+    return baseConfig;
 };
